1f20933e56319d677a8d14d594b9a043831f89e7
Cerek Hillen <chillen@lyft.com>
2021-02-02 21:42:32 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# taskn
+
+TODO
---
+++ src/main.rs
@@ -0,0 +1,65 @@
+use std::env;
+use std::io;
+use std::process::Command;
+use std::str;
+
+use serde::Deserialize;
+use serde_json;
+
+fn main() -> io::Result<()> {
+    let args = env::args().skip(1).collect::<Vec<String>>();
+    let output = Command::new("task").arg("export").args(args).output()?;
+    let output = String::from_utf8(output.stdout).unwrap();
+    let tasks = serde_json::from_str::<Vec<Task>>(&output).unwrap();
+
+    let status = Command::new("mkdir")
+        .arg("-p")
+        // TODO: expand this tilde so it's not pointing to the verbatim ~ directory
+        .arg("~/.taskn")
+        .output()?
+        .status;
+    if !status.success() {
+        // TODO: handle error
+    }
+
+    let status = Command::new("nvim")
+        .args(
+            tasks
+                .into_iter()
+                .filter(|task| task.status.is_active())
+                .map(|task| format!("~/.taskn/{}.md", task.uuid))
+                .collect::<Vec<String>>(),
+        )
+        .status()?;
+    if !status.success() {
+        // TODO: handle error
+    }
+
+    Ok(())
+}
+
+#[derive(Debug, Deserialize)]
+struct Task {
+    id: usize,
+    uuid: String,
+    status: Status,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "lowercase")]
+enum Status {
+    Pending,
+    Waiting,
+    Completed,
+    Deleted,
+}
+
+impl Status {
+    fn is_active(&self) -> bool {
+        use Status::*;
+        match self {
+            Pending | Waiting => true,
+            _ => false,
+        }
+    }
+}
