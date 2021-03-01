ee2dac8b77d090bee5ef183d6180b21dfbcf3b15
Cerek Hillen <chillen@lyft.com>
2021-02-28 19:32:24 -0800

    batch writes in a nasty way

--- src/commands/remind/mod.rs
+++ src/commands/remind/mod.rs
@@ -11,16 +11,19 @@ pub fn execute(opt: Opt) -> io::Result<()> {
     taskwarrior_args.push("+remindme".to_string());
     taskwarrior_args.push("(status:pending or status:waiting)".to_string());
     let tasks = Task::get(taskwarrior_args.into_iter())?;
+    let task_len = tasks.len();
 
     let mut event_store = EventStore::new().unwrap();
-    for task in tasks.into_iter() {
+    for (i, task) in tasks.into_iter().enumerate() {
         let reminder = Reminder::new(
             &mut event_store,
             task.description,
             task.uuid,
             task.wait.map(|pdt| pdt.0),
         );
-        event_store.save_reminder(reminder, true).unwrap();
+        event_store
+            .save_reminder(reminder, i == task_len - 1)
+            .unwrap();
     }
 
     Ok(())
