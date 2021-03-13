20a2623d2da54a1e95ea0264621a7296882d2bf9
Cerek Hillen <chillen@lyft.com>
2021-03-13 13:09:03 -0800

    feature brainstorm

--- src/commands/interactive/mod.rs
+++ src/commands/interactive/mod.rs
@@ -12,6 +12,30 @@ use crate::opt::Opt;
 use crate::taskwarrior::Task;
 use events::{Event, Events};
 
+// !!!feature brainstorm!!!
+//
+// - modal UI; fewer keystrokes = faster interaction.
+//   after using a UI for a while, you can learn how to interact
+//   so you no longer need to type out full commands
+//   - normal mode
+//     - up + down to nagivate between notes
+//     - enter to open up $EDITOR on the taskn note
+//     - "m" or "e" to enter modify/edit mode (which one?)
+//     - "a" to enter add mode
+//   - edit mode
+//     - ESC / Ctrl-F to exit edit more
+//     - r to toggle +remindme tag
+//     - e change the estimate
+//     - u to change urgency
+//     - p to change project
+//     - t to add/remove tag (normal +/- taskwarrior syntax)
+//
+// - try to build out a joint estimate + urgency ordering system
+//   so that tasks have a consistent order and i can capture
+//   top-to-bottom
+//
+// - preview taskn notes when you select a task
+
 pub fn execute(opt: Opt) -> io::Result<()> {
     let stdout = io::stdout().into_raw_mode()?;
     let backend = TermionBackend::new(stdout);
